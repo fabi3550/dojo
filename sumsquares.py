0d393dcb91ff4b2805242e867dd95b7d45209e10
Fabian Taubald <fabian@fabiantaubald.de>
2019-07-24 20:20:21 +0200

    codewars faehig

--- sumsquares.py
+++ sumsquares.py
@@ -4,11 +4,14 @@ import sys
 class SumSquares(object):
 
   def __init__(self, value):
+      self.compose(n)
 
-      init_square = value**2
+  def compose(self, n):
+
+      init_square = n**2
       temp_square = init_square
 
-      i = value - 1
+      i = n - 1
       squares = []
 
       while i > 0:
@@ -17,16 +20,18 @@ class SumSquares(object):
           if (temp_square > 0):
               squares.append(i)
           elif (temp_square == 0):
-              # print(temp_square)
               break
           else:
               temp_square += i**2
               i -= 1
-
           i -= 1
 
-      if (len(squares) > 0):
-          print(squares)
+           if (len(squares) > 0):
+               return_value = squares
+           else:
+               return_value = None
+
+       return return_value
 
 if __name__ == "__main__":
 
